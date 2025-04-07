332d377b343f6fd6a325623c9e3583ffdc798fbb
Koshy <55425558+ashish-koshy@users.noreply.github.com>
2025-04-08 03:15:53 +0530

    Create k8-worker-init.sh

---
+++ k8-worker-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Default values
+DEFAULT_HOST_NAME="k8sworker1.example.net"
+
+# Parse command line arguments
+HOST_NAME=${1:-$DEFAULT_HOST_NAME}
+
+log() {
+  echo "$1"
+}
+
+log "Setting hostname to $HOST_NAME..."
+hostnamectl set-hostname "$HOST_NAME"
+
+log "Worker initialized..."

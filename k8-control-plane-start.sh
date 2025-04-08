5f4ae56a0a0c24419b42635900d2503a887e3255
Koshy <55425558+ashish-koshy@users.noreply.github.com>
2025-04-08 06:09:11 +0530

    Update k8-control-plane-start.sh

--- k8-control-plane-start.sh
+++ k8-control-plane-start.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-# Default values
 DEFAULT_CALICO_VERSION="v3.29.3"
-
-# Parse command line arguments
 CALICO_VERSION=${1:-$DEFAULT_CALICO_VERSION}
 
-log() {
-  echo "$1"
-}
-
-log "Calico version selection : $CALICO_VERSION"
+echo "Calico version selection : $CALICO_VERSION"
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -23,4 +16,4 @@ kubectl get nodes
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/${CALICO_VERSION}/manifests/calico.yaml
 kubectl get pods -n kube-system
 
-log "Control plane is ready for worker nodes..."
+echo "Control plane is ready for worker nodes..."

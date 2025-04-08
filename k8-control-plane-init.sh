97f271be1cee4b445e974b6386521e72942a6edc
Koshy <55425558+ashish-koshy@users.noreply.github.com>
2025-04-08 06:08:34 +0530

    Update k8-control-plane-init.sh

--- k8-control-plane-init.sh
+++ k8-control-plane-init.sh
@@ -3,21 +3,14 @@ set -e
 
 source ./check-root.sh
 
-# Default values
 DEFAULT_HOST_NAME="k8smaster.example.net"
-
-# Parse command line arguments
 HOST_NAME=${1:-$DEFAULT_HOST_NAME}
 
-log() {
-  echo "$1"
-}
-
-log "Setting hostname to $HOST_NAME..."
+echo "Setting hostname to $HOST_NAME..."
 hostnamectl set-hostname "$HOST_NAME"
 
 # Initialize control plane
-log "Initializing Kubernetes control plane..."
+echo "Initializing Kubernetes control plane..."
 kubeadm init --control-plane-endpoint="$HOST_NAME"
 
-log "Control plane initialized..."
+echo "Control plane initialized..."
